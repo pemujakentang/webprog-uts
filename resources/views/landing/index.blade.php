2c2bd5202345d9dc98ba3e2b71fdd6256bd870e9
nasbi24 <nasbi24@gmail.com>
2023-10-22 13:14:36 +0700

    revisi dikit lg fix

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -19,7 +19,7 @@
 <div class="w-full h-screen overflow-scroll md:overflow-hidden md:justify-normal justify-end flex flex-col md:flex-row">
 
     <div class="h-full overflow-hidden flex items-end w-full md:w-[23%] flex-col md:justify-normal justify-end"> 
-            <div class="w-[40%] h-[25%] flex flex-col md:hidden mx-auto z-10">
+            <div class="w-[40%] h-[25%] max-w-[150px] flex flex-col md:hidden mx-auto z-10">
                 <image class="" src="/images/pizzalogo.webp" alt=""></image>
             </div>
 
@@ -33,11 +33,11 @@
             </div>
 
             <div class="h-[65%] md:h-full w-full flex justify-center flex-col z-10">
-                <div class="h-[20%] md:h-24 flex flex-col justify-center w-full -rotate-[12.5deg] bg-[#ffc013] scale-[1.15] hover:scale-[1.3] duration-75 hover:font-extrabold hover:bg-[#616161] cursor-pointer mb-5" href="/"><p class="text-3xl text-white duration-75 font-bungeeregular text-center">MENU</p></div>
+                <div class="h-[20%] md:h-24 flex flex-col justify-center w-full -rotate-[12.5deg] bg-[#ffc013] scale-[1.15] hover:scale-[1.3] duration-75 hover:font-extrabold hover:bg-[#616161] cursor-pointer mb-6 md:mb-5" href="/"><p class="text-3xl text-white duration-75 font-bungeeregular text-center">MENU</p></div>
 
-                <div class="h-[20%] md:h-24 flex flex-col justify-center w-full -rotate-[12.5deg] bg-[#ffc013]  scale-[1.15] hover:scale-[1.3] duration-75 hover:font-extrabold hover:bg-[#616161] cursor-pointer mb-5" href="/"><p class="text-3xl text-white duration-75 font-bungeeregular text-center">MY ORDER</p></div>
+                <div class="h-[20%] md:h-24 flex flex-col justify-center w-full -rotate-[12.5deg] bg-[#ffc013]  scale-[1.15] hover:scale-[1.3] duration-75 hover:font-extrabold hover:bg-[#616161] cursor-pointer mb-6 md:mb-5" href="/"><p class="text-3xl text-white duration-75 font-bungeeregular text-center">MY ORDER</p></div>
 
-                <div class="h-[20%] md:h-24 flex flex-col justify-center w-full -rotate-[12.5deg] bg-[#ffc013]  scale-[1.15] hover:scale-[1.3] duration-75 hover:font-extrabold hover:bg-[#616161] cursor-pointer z-10 mb-5 md:mb-[60px] " href="/"><p class="text-3xl text-white duration-75 font-bungeeregular text-center">CONTACT US</p></div>
+                <div class="h-[20%] md:h-24 flex flex-col justify-center w-full -rotate-[12.5deg] bg-[#ffc013]  scale-[1.15] hover:scale-[1.3] duration-75 hover:font-extrabold hover:bg-[#616161] cursor-pointer z-10 mb-6 md:mb-[60px] " href="/"><p class="text-3xl text-white duration-75 font-bungeeregular text-center">CONTACT US</p></div>
                 
                 <div class="h-[20%] md:h-24 md:hidden flex flex-col justify-center w-full -rotate-[12.5deg] bg-[#ffc013]  scale-[1.15] hover:scale-[1.3] duration-75 hover:font-extrabold hover:bg-[#616161] cursor-pointer z-10 mb-[60px] md:mb-[47px] " href="/"><p class="text-3xl text-white duration-75 font-bungeeregular text-center">SIGN IN</p></div>
 
